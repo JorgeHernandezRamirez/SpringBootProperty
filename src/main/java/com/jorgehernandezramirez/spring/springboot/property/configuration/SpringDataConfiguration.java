dc9cb8734922bf80158084f284a4bafaf50d45b4
Jorge Hernández Ramírez <jorgehernandezramirez@MacBook-Pro-de-Jorge.local>
2017-03-19 12:20:42 +0100

    no message

--- src/main/java/com/jorgehernandezramirez/spring/springboot/property/configuration/SpringDataConfiguration.java
+++ src/main/java/com/jorgehernandezramirez/spring/springboot/property/configuration/SpringDataConfiguration.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Configuration
 @ConfigurationProperties("spring.springdata.mongodb")
-public class SpringDataConfiguration implements Serializable {
+public class SpringDataConfiguration {
 
     private Boolean relational;
 
